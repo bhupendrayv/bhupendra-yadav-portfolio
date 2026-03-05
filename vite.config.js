fefe20ace05250176c2eedf32308b22e414c5099
Bhupendra Yadav <bhupendrayadav2077@gmail.com>
2026-03-05 21:48:42 +0530

    Initial portfolio commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bhupendra-yadav-portfolio-main/',
+  base: '/bhupendra-yadav-portfolio/',
 })
